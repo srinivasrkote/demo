daded3400ca3370d0143d97af2a8a94382b11381
srinivas <sri202177@gmail.com>
2025-09-09 18:08:28 +0530

    commit message

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Mysore!"
+    return "Hello Mysore,karnataka!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
